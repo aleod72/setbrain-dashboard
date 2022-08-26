7d976a9cc4e893a05e01868bd7cbfc00726852ed
LINGUET Emmanuel <emmanuel.linguet@setbrain.fr>
2022-08-26 17:39:10 +0200

    setup profile service

---
+++ libs/shared/data-access/users/interfaces/profile.interface.ts
@@ -0,0 +1,8 @@
+export interface Profile {
+  id: string;
+  updated_at: string;
+  firstname: string;
+  lastname: string;
+  email: string;
+  avatar_url: string;
+}
--- libs/shared/data-access/users/src/index.ts
+++ libs/shared/data-access/users/src/index.ts
@@ -1,1 +1,2 @@
 export * from './lib/shared-data-access-users.module';
+export * from './lib/profile.service';
