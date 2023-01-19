f2ad6883a8e0cd69113e511f7e9fe6f293c9f691
xmanu <xmanu.linguet@gmail.com>
2023-01-19 15:12:25 +0100

    feature(data-access): tasks

---
+++ libs/shared/data-access/tasks/src/index.ts
@@ -0,0 +1,2 @@
+export * from './lib/tasks.service';
+export * from './lib/interfaces/task.interface';
---
+++ libs/shared/data-access/tasks/src/lib/tasks.service.ts
@@ -0,0 +1,41 @@
+import { Injectable } from '@angular/core';
+import {SupabaseService} from "@setbrain-dashboard/shared/data-access/database";
+import { Task } from "./interfaces/task.interface";
+
+@Injectable({
+  providedIn: 'root'
+})
+export class TasksService {
+
+  constructor(private supabaseService: SupabaseService) {}
+
+  async getAllTasks() {
+    const {error, data} = await this.supabaseService.supabase.from('tasks').select('*');
+    if(error) throw error;
+    return data as Task[];
+  }
+
+  async updateTask(projectId: string, values: object) {
+    const {error} = await this.supabaseService.supabase
+      .from('tasks')
+      .update(values)
+      .eq('id', projectId);
+    if(error) throw error;
+  }
+
+  async createTask(task: Task) {
+    const { data, error } = await this.supabaseService.supabase
+      .from('tasks')
+      .insert([task]);
+    if(error) throw error;
+    return data;
+  }
+
+  async deleteTask(taskID: string) {
+    const { error } = await this.supabaseService.supabase
+      .from('tasks')
+      .delete()
+      .eq('id', taskID);
+    if(error) throw error;
+  }
+}
