ffbb09983621143712e2c4961735f68da0077ee6
xmanu <xmanu.linguet@gmail.com>
2023-01-19 14:02:04 +0100

    Add projects data-access library

---
+++ libs/shared/data-access/projects/src/lib/projects.service.ts
@@ -0,0 +1,41 @@
+import { Injectable } from '@angular/core';
+import {SupabaseService} from "@setbrain-dashboard/shared/data-access/database";
+import {Project} from "./interfaces/project.interface";
+
+@Injectable({
+  providedIn: 'root'
+})
+export class ProjectsService {
+
+  constructor(private supabaseService: SupabaseService) {}
+
+  async getAllProjects() {
+    const {error, data} = await this.supabaseService.supabase.from('projects').select('*');
+    if(error) throw error;
+    return data as Project[];
+  }
+
+  async updateProject(projectId: string, values: object) {
+    const {error} = await this.supabaseService.supabase
+      .from('projects')
+      .update(values)
+      .eq('id', projectId);
+    if(error) throw error;
+  }
+
+  async createProject(project: Project) {
+    const { data, error } = await this.supabaseService.supabase
+      .from('projects')
+      .insert([project]);
+    if(error) throw error;
+    return data;
+  }
+
+  async deleteProject(projectID: string) {
+    const { error } = await this.supabaseService.supabase
+      .from('projects')
+      .delete()
+      .eq('id', projectID);
+    if(error) throw error;
+  }
+}
