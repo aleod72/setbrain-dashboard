35ecc120901550483d284c35a248f5aadb81ddca
Emmanuel LINGUET <emmanuel.linguet@setbrain.fr>
2023-01-19 21:09:57 +0000

    fix(ui): some components were broken

--- libs/shared/ui/src/lib/button/button.component.ts
+++ libs/shared/ui/src/lib/button/button.component.ts
@@ -14,6 +14,7 @@ import {
 })
 export class ButtonComponent {
   @Input() label: string | undefined;
+  @Input() full? = false;
   @Output() buttonClicked = new EventEmitter();
 
   onClick(clickEvent: MouseEvent) {
