e315d53ea6e46a44d176ad01366122bd37000501
Emmanuel LINGUET <emmanuel.linguet@setbrain.fr>
2023-01-25 14:08:21 +0000

    fix(ui): Fix tab-bar project link three

--- libs/features/base/src/lib/lib.routes.ts
+++ libs/features/base/src/lib/lib.routes.ts
@@ -4,5 +4,11 @@ import {AuthGuard} from "@setbrain-dashboard/features/guards";
 
 export const featuresBaseRoutes: Route[] = [
   {path: '', component: BaseComponent, canActivate: [AuthGuard]},
-  {path: ':id', component: BaseComponent}
+  {path: ':id', component: BaseComponent, children: [
+      {path: 'home', redirectTo: '', pathMatch: 'full'},
+      {path: 'tasks', component: BaseComponent},
+      {path: 'admin', component: BaseComponent},
+      {path: 'stats', component: BaseComponent},
+      {path: 'files', component: BaseComponent}
+    ]},
 ];
