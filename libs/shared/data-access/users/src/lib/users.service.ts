69a097b3a88ba1b7e1c65702234df4c561c95f61
Emmanuel LINGUET <emmanuel.linguet@setbrain.fr>
2023-01-26 17:21:49 +0000

    feature(data-access): Add Users service

---
+++ libs/shared/data-access/users/src/lib/users.service.ts
@@ -0,0 +1,27 @@
+import { Injectable } from '@angular/core';
+import {SupabaseService} from "@setbrain-dashboard/shared/data-access/database";
+import {BannerProfile, Profile} from "@setbrain-dashboard/shared/data-access/users";
+
+@Injectable({
+  providedIn: 'root'
+})
+export class UsersService {
+
+  constructor(private supabaseService: SupabaseService) {}
+
+  async getUserProfile(userId: string) {
+    const {data, error} = await this.supabaseService.supabase.from('profiles')
+      .select('*')
+      .eq('id', userId);
+    if(error) throw error;
+    return data[0] as Profile;
+  }
+
+  async getBannerOfUsers(userIds: string[]) {
+    const {data, error} = await this.supabaseService.supabase.from('profiles')
+      .select('id, firstname, lastname, avatar_url')
+      .in('id', userIds);
+    if(error) throw error;
+    return data as unknown as BannerProfile[];
+  }
+}
