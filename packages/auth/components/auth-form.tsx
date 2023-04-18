adee00f91f8983c31b27f76351232f9814c5e613
Emmanuel LINGUET <emmanuel.linguet@setbrain.fr>
2023-04-18 20:03:36 +0000

    fix: redirect url with doc

--- packages/auth/components/auth-form.tsx
+++ packages/auth/components/auth-form.tsx
@@ -4,10 +4,19 @@ import * as React from 'react';
 import { Button, Input } from 'ui';
 import { useSupabase } from '../providers/supabase-provider';
 
-interface IAuthFormProps {
-}
+const getURL = () => {
+  let url =
+    process?.env?.NEXT_PUBLIC_SITE_URL ?? // Set this to your site URL in production env.
+    process?.env?.NEXT_PUBLIC_VERCEL_URL ?? // Automatically set by Vercel.
+    'http://localhost:3000/';
+  // Make sure to include `https://` when not localhost.
+  url = url.includes('http') ? url : `https://${url}`;
+  // Make sure to including trailing `/`.
+  url = url.charAt(url.length - 1) === '/' ? url : `${url}/`;
+  return url;
+};
 
-const AuthForm: React.FunctionComponent<IAuthFormProps> = (props) => {
+const AuthForm = () => {
   const { supabase } = useSupabase();
   const [email, setEmail] = React.useState('');
   const [password, setPassword] = React.useState('');
@@ -16,11 +25,7 @@ const AuthForm: React.FunctionComponent<IAuthFormProps> = (props) => {
     await supabase.auth.signInWithOAuth({
       provider: 'google',
       options: {
-        redirectTo: `${
-          process.env.NEXT_PUBLIC_VERCEL_URL + '/'
-            ? 'https://' + process.env.NEXT_PUBLIC_VERCEL_URL + '/'
-            : 'http://localhost:3000/'
-        }`
+        redirectTo: getURL()
       }
     })
   }
