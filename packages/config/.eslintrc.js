fc26def8b2a21ea023aff3d6e3760cc099877f70
Emmanuel LINGUET <emmanuel.linguet@setbrain.fr>
2023-05-08 08:43:24 +0000

    fix eslint rules

--- packages/config/.eslintrc.js
+++ packages/config/.eslintrc.js
@@ -1,6 +1,5 @@
 module.exports = {
   extends: ["next", "prettier", "next/core-web-vitals"],
-  plugins: ["simple-import-sort", "prettier"],
   overrides: [
     {
       files: ["*.ts", "*.tsx", "*.js", "*.jsx"],
@@ -12,16 +11,14 @@ module.exports = {
     },
   },
   rules: {
-    "simple-import-sort/imports": "error",
-    "simple-import-sort/exports": "error",
     "no-unused-vars": "error",
     "prefer-const": "error",
     "no-irregular-whitespace": "error",
     "no-trailing-spaces": "error",
-    semi: "error",
     "no-empty-function": "error",
     "no-duplicate-imports": "error",
     "newline-after-var": "error",
-    camelcase: "error"
+    "semi": ["error", "always"],
+    "@next/next/no-html-link-for-pages": "off",
   },
 };
